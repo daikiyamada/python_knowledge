c82791a2516a185b9eaea8fa7f700b9e282fc244
yamadadaiki <daiki.baseball0516@gmail.com>
2020-10-11 15:05:43 +0900

    1012commit

--- newral_1012.py
+++ newral_1012.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib.pylab as plt
 
 """
-step_function
------------
-@param {array} np.array()　int型の配列をreturnする
+step_function:ステップ関数
+
+sigmoid：シグモイド関数
 """
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def ReLU(x):
+    return np.maximum(0,x)
 
 """
 description
@@ -18,4 +24,14 @@ x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1) # y 軸の範囲を指定 plt.show()
+plt.show()
+
+y2=sigmoid(x)
+plt.plot(x,y2)
+plt.ylim(-0.1,1.1)
+plt.show()
+
+y = ReLU(x)
+plt.plot(x,y)
+plt.ylim(-0.1,5)
 plt.show()
